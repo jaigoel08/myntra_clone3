38bc5f6e97d5e55fbb6864d05982ab13f2c63d35
jaigoel08 <jaigoel14@gmail.com>
2024-06-01 23:30:29 +0530

    commited successfully

---
+++ item.js
@@ -0,0 +1,179 @@
+const items = [
+    {
+        id: '001',
+        image:'image/earrings.jpg',
+        company: 'Carlton London',
+        item_name: 'Rhodium-Plated CZ Floral Studs',
+        original_price: 1045,
+        current_price: 606,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: '10 june 2024',
+        rating: {
+            stars: 4.5,
+            count: 1400,
+        },
+    },{
+        id:'002',
+        image:'image/download.jpg',
+        company: 'US Polo Assain',
+        item_name: 'US Polo Collar Neck Mens T-Shirt',
+        original_price: 1500,
+        current_price: 1200,
+        discount_percentage: 30,
+        return_period: 14,
+        delivery_date: '15 june 2024',
+        rating: {
+            stars: 3.9,
+            count: 1800,
+        },
+    },{
+      id:'003',
+      image:'image/gown.jpg',
+      company: 'Louis Vuitton',
+      item_name: 'Red Color Women Gown',
+      original_price: 7000,
+      current_price: 5000,
+      discount_percentage: 30,
+      return_period: 14,
+      delivery_date: '12 june 2024',
+      rating: {
+         stars: 4.8,
+         count: 2500,
+    },
+},{
+    id:'004',
+    image:'image/pant.jpg',
+    company: 'H&M',
+    item_name: 'Blue Mens Formal Pants',
+    original_price: 700,
+    current_price: 500,
+    discount_percentage: 30,
+    return_period: 14,
+    delivery_date: '12 june 2024',
+    rating: {
+       stars: 4.0,
+       count: 1220,
+    },
+}, {
+    id:'005',
+    image:'image/jean.jpg',
+    company: 'H&M',
+    item_name: 'Blue Colored Women Straight Jeans',
+    original_price: 2100,
+    current_price: 1800,
+    discount_percentage: 50,
+    return_period: 14,
+    delivery_date: '12 june 2024',
+    rating: {
+       stars: 4.2,
+       count: 1700,
+    }
+},
+    { 
+     id:'006',
+     image:'image/team.jpg',
+     company: 'Adidas',
+     item_name: 'Indian Team t-20 Jersey',
+     original_price: 2500,
+     current_price: 1500,
+     discount_percentage: 45, 
+     return_period: 14,
+     delivery_date: '12 june 2024',
+     rating: {
+         stars: 4.7,
+         count: 2500,
+   }
+},
+   {
+    id:'007',
+    image:'image/fogg.jpg',
+    company: 'FOGG',
+   item_name: 'EAU DE PARFUM',
+   original_price: 1500,
+   current_price: 900,
+   discount_percentage: 43,
+   return_period: 14,
+   delivery_date: '12 june 2024',
+   rating: {
+      stars: 0.0,
+      count: 0,
+    }
+},
+      
+      {
+        id:'008',
+        image:'image/makeup.jpg',
+      company: 'MAYBELLINE',
+      item_name: 'Women Facial Make-up Kit',
+      original_price: 17000,
+      current_price: 9000,
+      discount_percentage: 45,
+      return_period: 14,
+      delivery_date: '12 june 2024',
+      rating: {
+         stars: 4.9,
+         count: 1100,}},
+        
+         {
+            id:'009',
+            image:'image/nike.jpg',
+            company: 'NIKE',
+            item_name: 'Air Jordans',
+            original_price: 25000,
+            current_price: 19000,
+            discount_percentage: 45,
+            return_period: 14,
+            delivery_date: '18 june 2024',
+            rating: {
+                 stars: 4.9,
+                count: 900, 
+               }
+           },
+           {
+            id:'10',
+            image:'image/bata.jpg',
+            company: 'BATA',
+            item_name: 'Leather Formal Shoes For Mens',
+            original_price:2000 ,
+            current_price:1349 ,
+            discount_percentage: 37,
+            return_period: 7,
+            delivery_date: '17 june 2024',
+            rating: {
+                 stars: 4.9,
+                count: 2000, 
+               }
+           },
+           {
+            id:'11',
+            image:'image/apple.jpg',
+            company: 'APPLE',
+            item_name: 'Wireless Bluetooth Headphones',
+            original_price: 75000,
+            current_price: 49999,
+            discount_percentage: 50,
+            return_period: 14,
+            delivery_date: '12 june 2024',
+            rating: {
+                 stars: 4.9,
+                count: 1100, 
+               }
+           },
+           {
+            id:'12',
+            image:'image/boat.jpg',
+            company: 'BOAT',
+            item_name: 'Boat Gaming Headphones',
+            original_price: 10000,
+            current_price: 7999,
+            discount_percentage: 20,
+            return_period: 10,
+            delivery_date: '06 june 2024',
+            rating: {
+                 stars: 4.9,
+                count: 1800, 
+               }
+           },
+
+];
